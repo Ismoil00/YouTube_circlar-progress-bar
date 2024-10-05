d9fc2214652cc497d673036f320ee51a3f7b4020
Ismoil Sharifov <sharipovismoil01@gmail.com>
2024-10-05 22:04:46 +0500

    initiation

---
+++ test.js
@@ -0,0 +1,16 @@
+const number = document.getElementById("number");
+let counter = 0;
+
+initApp();
+
+function initApp() {
+  setInterval(() => {
+    if (counter === 65) {
+      clearInterval();
+      return;
+    }
+
+    counter++;
+    number.innerText = counter + "%";
+  }, 1000 / 65);
+}
